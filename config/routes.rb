4123218930ab1f23a7ed8abc0c2caa01ab98d971
EC2 Default User <ec2-user@ip-172-31-14-67.ec2.internal>
2022-08-20 03:39:25 +0000

    [Update]バリデーションメッセージ表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root :to => "homes#top"
-  get 'books/new'
   get 'books'=>'books#index'
-
   post 'books' => 'books#create'
   get '/top' => "homes#top"
   get 'books/:id' => 'books#show', as: 'book'
